ca85a495ccd14865ed98f36871c4bba3d41be00d
wasim <mdwasim6123@gmail.com>
2023-08-14 01:29:09 +0530

    second commiyt

--- router/web.js
+++ router/web.js
@@ -3,7 +3,7 @@ const express = require('express')
 const homeController = require('../controller/homeController.js')
 
 const route = express.Router()
-route.get('/home', homeController.api)
+route.get('/', homeController.api)
 route.post('/search', homeController.apisearch)
 
 module.exports = route
